5f83265080eb564deffd4ba6079fcb8d0fc72c90
rohinilri <30829250+rohinilri@users.noreply.github.com>
2017-09-05 16:16:19 +0530

    Create l.c

---
+++ l.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter a number");
+scanf("%d",&a);
+if(a%2==0)
+{
+pintf("the number is even");
+}
+else
+{
+printf('the number is odd");
+}
+}
