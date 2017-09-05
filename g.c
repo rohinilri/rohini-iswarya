56c742340ce654ad1cf30b9e99a29545e9939217
rohinilri <30829250+rohinilri@users.noreply.github.com>
2017-09-05 16:04:16 +0530

    Create g.c

---
+++ g.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int a;
+printf("enter a number");
+scanf("%d",&a);
+if(a>0)
+printf("a is positive number");
+else
+printf("a is negative number");
+getch();
+}
