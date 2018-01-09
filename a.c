a82d8841cc3a0cf271a4a69995d09e88c27cf348
rohinilri <30829250+rohinilri@users.noreply.github.com>
2018-01-09 16:03:44 +0530

    Create a.c

---
+++ a.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char c;
+printf("enter a character ");
+scanf("%d",&g);
+if(C>=A && C<=Z // C>=a && c<=z)
+printf("g is an alphabet");
+else
+printf("gis not an alphabet");
+}
