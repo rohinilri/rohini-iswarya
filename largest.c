c78e0ce4f742158049a26701231726125b9f5575
rohinilri <30829250+rohinilri@users.noreply.github.com>
2018-01-09 16:13:03 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a=5,b=6,c=7;
+if((a>b)&&(a>c))
+printf("a is the largest number");
+elseif((a<b)&&(b>c))
+printf("b is the largest number");
+else
+printf("c is the largest number");
+}
