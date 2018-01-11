2d28d458a860191ddb18824b79b7fb3ebdf0ef12
rohinilri <30829250+rohinilri@users.noreply.github.com>
2018-01-11 16:51:12 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+int n,k,sum;
+printf("enter the value of n");
+scanf("%d",&n);
+printf("enter the value of k");
+scanf("%d",&k);
+sum=pow(n,k);
+printf("the sum value is");
+}
