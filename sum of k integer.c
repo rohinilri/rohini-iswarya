0ed6d2eb3b827d75e15edd89dac70a0f3969b2bc
rohinilri <30829250+rohinilri@users.noreply.github.com>
2018-01-11 16:31:26 +0530

    Create sum of k integer.c

---
+++ sum of k integer.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+int a[5]={1,2,3,4,5},k,n,sum=o;
+printf("enter the value of n");
+scanf("%d",&n);
+for(i=1;i<=5;i++)
+{
+printf("%d",a[i]);
+}
+for(i=1;i<=k,i++)
+{
+sum=sum+n[i];
+}
+pritf("the sum value is.");
+}
