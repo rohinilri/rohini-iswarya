84d96c1c74a467ede2b1c3e030320c4c34588602
rohinilri <30829250+rohinilri@users.noreply.github.com>
2018-01-19 16:18:45 +0530

    Create even no bet 2 inte.c

---
+++ even no bet 2 inte.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int i,n,num;
+printf("enter the number for n and num");
+scanf("%d%d",&n,&num);
+for(i=n;i<=num;i++)
+{
+if(i%2==0)
+printf("the even numbers between two intervals");
+}
+}
