1b5cfba5249dc18ec3b615ea19e8f1273640b2a6
rohinilri <30829250+rohinilri@users.noreply.github.com>
2018-01-19 16:08:14 +0530

    Create odd num bw 2 inte.c

---
+++ odd num bw 2 inte.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int i,m,num;
+printf("enter the number for m and num");
+scanf("%d%d",&m,&num);
+for(i=m,i<=num,i++)
+{
+if(i%2==1)
+printf("the odd numbers between two intervals are");
+}
+}
