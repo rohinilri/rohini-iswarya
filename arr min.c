b3dcb09dd92cbea9f13ef8a5b39c98f9792fa8e7
rohinilri <30829250+rohinilri@users.noreply.github.com>
2018-01-21 12:19:54 +0530

    Create arr min.c

---
+++ arr min.c
@@ -0,0 +1,20 @@
+#include<stdio.h> 
+int main() 
+{
+   int a[30],i,num,min; 
+   printf("Enter no of elements");
+   scanf("%d", &num);  
+   printf("Enter the elements")
+   scanf("%d", &a[i]);
+   }
+   min=a[i]; 
+   for(i=0;i<n;i++) 
+   {
+      if(a[i]>min)
+      {
+         min= a[i];
+      }
+   } 
+   printf("min Element",max); 
+   return 0;
+}
