a87b771878dc010f282ff7bc785d1ab7c9175bb5
rohinilri <30829250+rohinilri@users.noreply.github.com>
2018-01-21 12:16:05 +0530

    Create arr max.c

---
+++ arr max.c
@@ -0,0 +1,20 @@
+#include<stdio.h> 
+int main() 
+{
+   int a[30],i,num,max; 
+   printf("Enter no of elements");
+   scanf("%d", &n);  
+   printf("Enter the elements")
+   scanf("%d", &a[i]);
+   }
+   max=a[i]; 
+   for(i=0;i<num;i++) 
+   {
+      if(a[i]>max)
+      {
+         max= a[i];
+      }
+   } 
+   printf("max Element",max); 
+   return 0;
+}
