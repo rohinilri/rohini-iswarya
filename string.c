02e00e74bac0cb14c01990df07a6ebca3fc1ed2d
rohinilri <30829250+rohinilri@users.noreply.github.com>
2018-02-01 10:33:04 +0000

    Create string.c

---
+++ string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+char s[20];
+int i,k;
+printf("enter the value");
+scanf("%d",&k);
+printf("enter the character");
+scanf("%s",&s);
+for(i=1;i<=k;i++)
+{
+printf("\t %s",s);
+}
+}
