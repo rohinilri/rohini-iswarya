007407c8b81b58d5cbaa2b6d0296e37caa072b91
rohinilri <30829250+rohinilri@users.noreply.github.com>
2018-02-02 08:49:12 +0000

    Create within range.c

---
+++ within range.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n[10]={1,2,3,4,5,6,7,8,9,0},a;
+printf("enter the numbers:");
+scanf("%d",&a);
+if(a>1&&a<9)
+{
+printf("it is in the range");
+}
+else
+printf("it is not in range");
+}
