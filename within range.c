c6d1f5260ee50246a0908d16c17c6cd179c23a23
rohinilri <30829250+rohinilri@users.noreply.github.com>
2018-02-02 08:50:42 +0000

    Update within range.c

--- within range.c
+++ within range.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-int n[10]={1,2,3,4,5,6,7,8,9,0},a;
+int n[10]={1,2,3,4,5,6,7,8,9,10},a;
 printf("enter the numbers:");
 scanf("%d",&a);
-if(a>1&&a<9)
+if(a>1&&a<10)
 {
 printf("it is in the range");
 }
