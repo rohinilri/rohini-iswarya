d0ed278bb408f2ccb3410288a48025b4fdde1f8d
rohinilri <30829250+rohinilri@users.noreply.github.com>
2018-02-06 09:06:16 +0000

    Create n sum of all dig.c

---
+++ n sum of all dig.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+void main()
+{
+  int num,rem,sum=0;
+  printf("enter the number");
+  scanf("%d",&num);
+  while(num!=0)
+  {
+    rem=num%10;
+    sum=sum+rem;
+    num=num/10;
+  }
+  printf("%d",sum);
+}
