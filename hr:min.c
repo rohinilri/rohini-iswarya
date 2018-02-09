ff3a8b62a51e9be98dc9ba06f03d0c807b7b0c19
rohinilri <30829250+rohinilri@users.noreply.github.com>
2018-02-09 09:34:58 +0000

    Create hr:min.c

---
+++ hr:min.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int h1,h2,m1,m2,c,c1;
+printf("enter the h1&m1");
+scanf("%d%d",&h1,&m1);
+printf("enter the h2 &m2");
+scanf("%d%d",&h2,&m2);
+c=h1-m1;
+c1=h2-m2;
+
+printf("%d:%d",c,c1);
+}
