449c21d0a51981f3e25ee654b053eaef5da1e21e
rohinilri <30829250+rohinilri@users.noreply.github.com>
2018-03-01 16:34:56 +0530

    Update 58.c

--- 58.c
+++ 58.c
@@ -4,9 +4,9 @@ int main()
 int a,b;
 printf("enter the number for a and b");
 scanf("%d%d",&a,&b);
-printf("%d%d before swappig",a,b);
+printf("%d%d bfr swap",a,b);
 a=a^b;
 b=a^b;
 a=b^a;
-printf("\n %d%dafter swapping ",a,b);
+printf("\n %d%d aft swap ",a,b);
 }
