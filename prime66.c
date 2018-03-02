3a38c75da39be08b150bc9c863a9c1e7c619bee9
rohinilri <30829250+rohinilri@users.noreply.github.com>
2018-03-02 16:12:51 +0530

    Create prime66.c

---
+++ prime66.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n,i,c=0;
+printf("enter a number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+if(n%i==0)
+{
+c++;
+}
+}
+if(c==2)
+printf("it is a prime number");
+else
+printf("it is not a prime number");
+}
