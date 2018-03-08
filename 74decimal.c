06d235c9426237f7bcb73599d8b84addb6f6e5b8
rohinilri <30829250+rohinilri@users.noreply.github.com>
2018-03-08 16:20:41 +0530

    Create 74decimal.c

---
+++ 74decimal.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+
+void main()
+{
+    float num;
+    printf("Enter the number:");
+    scanf("%f",&num);
+    printf("The integer number after rounded is  %5.0f",num);
+  }
