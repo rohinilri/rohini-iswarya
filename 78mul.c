427c90775ffd78cce901bea128c45cf0c15d1fad
rohinilri <30829250+rohinilri@users.noreply.github.com>
2018-03-08 16:51:49 +0530

    Create 78mul.c

---
+++ 78mul.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int n, i=13;
+    printf("enter number:");
+    scanf("%d",&n);
+        if (n%i==0)
+            printf("yes");
+        else
+        printf("no");
+    }
