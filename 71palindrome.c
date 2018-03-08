dc61f1d41ebc10fdc3a12b5d6943a3a086a741cb
rohinilri <30829250+rohinilri@users.noreply.github.com>
2018-03-08 16:04:10 +0530

    Create 71palindrome.c

---
+++ 71palindrome.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char str[50],s1[50];
+printf("enter string:");
+scanf("%s",&str);
+if(s1==strrev(str))
+printf("yes it is palindrome");
+else
+printf("not a palindrome");
+}
