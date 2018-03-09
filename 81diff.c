085b51f89d3e22c5c01d8d3af0119548157e4707
rohinilri <30829250+rohinilri@users.noreply.github.com>
2018-03-09 16:33:17 +0530

    Create 81diff.c

---
+++ 81diff.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,diff;
+  printf("enter member in kabali and ninja:");
+  scanf("%d%d",&a,&b);
+  diff=b-a;
+  if(b>a)
+  {
+    printf("%d",diff);
+  }
+}
